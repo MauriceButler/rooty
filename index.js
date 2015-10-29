63812d09e1a8d23a2097213c95adc30a977d3151
mauricebutler <maurice.butler@gmail.com>
2015-10-29 17:02:57 +1000

    might work

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
 var caroot = require('caroot'),
     Module = require('module'),
     originalLoad = Module._load,
-    originalResolve = Module._resolveFilename;
+    originalResolve = Module._resolveFilename,
+    pathScopes = {},
+    defaultScope = '_default';
 
-function init(rootPath){
+function getPathScope(parent){
+    var pathScope = pathScopes[defaultScope],
+        parts = parent ? parent.filename.split('/') : [];
+
+    while(parts.length){
+        var currentScopePath = parts.join('/');
+
+        if(currentScopePath in pathScopes){
+            pathScope = pathScopes[currentScopePath];
+            break;
+        }
+
+        parts.pop();
+    }
+
+    return pathScope;
+}
+
+function init(rootPath, scope){
+    pathScopes[scope || defaultScope] = rootPath;
+
     Module._resolveFilename = function(path, passedModule){
-        return originalResolve(caroot(path, passedModule.filename, rootPath), passedModule);
+        var pathScope = getPathScope(passedModule);
+        return originalResolve(caroot(path, passedModule.filename, pathScope), passedModule);
     };
 
     Module._load = function(request, parent, isMain) {
-        return originalLoad(caroot(request, parent.filename, rootPath), parent, isMain);
+        var pathScope = getPathScope(parent);
+        return originalLoad(caroot(request, parent.filename, pathScope), parent, isMain);
     };
 }
 
